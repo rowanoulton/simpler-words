2dad00ebd5fc893af921d7513704fb46f15e5fbc
Rowan Oulton <rowan.oulton@gmail.com>
2015-09-09 16:53:36 +0200

    Test mixed case

--- test/basic.spec.js
+++ test/basic.spec.js
@@ -4,6 +4,7 @@
 
 var simplerWords = require('../index')
 var longWord = 'additional'
+var longWordMixedCase = 'aDditIONAl'
 var shortWord = 'more'
 var shortWords = ['more', 'extra']
 
@@ -12,6 +13,14 @@ describe('simpler-words', function () {
   describe('a long word', function () {
     it('should have suggestions', function () {
       var results = simplerWords(longWord)
+      expect(results.length).toEqual(2)
+      expect(results).toEqual(shortWords)
+    })
+  })
+
+  describe('a word of mixed case', function () {
+    it('should return the same suggestions', function () {
+      var results = simplerWords(longWordMixedCase)
       expect(results.length).toEqual(2)
       expect(results).toEqual(shortWords)
     })
